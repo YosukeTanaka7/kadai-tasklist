d52568f39d4c4ba3b9e869a6939a6d565e827e30
yosuke.tanaka <yosuke_t1210@i.softbank.jp>
2020-07-05 11:25:56 +0000

    add bootOK

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-(1..77).each do |number|
-  Task.create(status: '77個までステータス' + number.to_s, content: '結果' + number.to_s)
+(1..100).each do |number|
+  Task.create!(status: 'ステータス' + number.to_s, content: '結果' + number.to_s)
 end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
